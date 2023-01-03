ec089fdf00b17ff4a330ab299a14f588f0af2553
EndlessRecurrence <georgesut@yahoo.com>
2023-01-03 18:07:31 +0200

    Add a basic Jetpack Compose project with the image slider on the first screen

---
+++ MobilePedalPiApp/app/src/main/java/com/example/mobilepedalpiapp/MainActivity.kt
@@ -0,0 +1,116 @@
+package com.example.mobilepedalpiapp
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.Black
+import androidx.compose.ui.graphics.Color.Companion.LightGray
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.lerp
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.mobilepedalpiapp.ui.theme.MobilePedalPiAppTheme
+import com.example.mobilepedalpiapp.utils.guitarImages
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.calculateCurrentOffsetForPage
+import com.google.accompanist.pager.rememberPagerState
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.yield
+import kotlin.math.absoluteValue
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MobilePedalPiAppTheme {
+                AutoSliding()
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun AutoSliding() {
+    val pagerState = rememberPagerState()
+
+    LaunchedEffect(Unit) {
+        while (true) {
+            yield()
+            delay(2000)
+            pagerState.animateScrollToPage(
+                page = (pagerState.currentPage + 1) % guitarImages,
+                animationSpec = tween(600)
+            )
+        }
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Row(
+            modifier = Modifier
+                .weight(1f),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            HorizontalPager(
+                state = pagerState,
+                modifier = Modifier
+                    .weight(1f),
+                count = guitarImages
+            ) { page ->
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(LightGray)
+                    ) {
+                        Image(
+                            painter = painterResource(
+                                id = when (page) {
+                                    0 -> R.drawable.sg
+                                    1 -> R.drawable.gretsch
+                                    2 -> R.drawable.strat
+                                    else -> R.drawable.sg
+                                }
+                            ),
+                            contentDescription = "Image",
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier.fillMaxSize()
+                        )
+                    }
+                }
+            }
+        }
+
+        Row(
+            modifier = Modifier
+                .weight(2f),
+            horizontalArrangement = Arrangement.Center
+        ) {}
+
+    }
+}
---
+++ MobilePedalPiApp/app/src/main/java/com/example/mobilepedalpiapp/utils/Constants.kt
@@ -0,0 +1,3 @@
+package com.example.mobilepedalpiapp.utils
+
+val guitarImages = 3;
