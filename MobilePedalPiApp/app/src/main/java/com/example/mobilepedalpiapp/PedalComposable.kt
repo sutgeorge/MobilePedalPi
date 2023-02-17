1ff90d4fc5c01e66ceb904b31ac66c211e4d96c1
EndlessRecurrence <georgesut@yahoo.com>
2023-02-17 15:16:51 +0200

    Add the pedal template with the SwitchComposable placed on top

--- MobilePedalPiApp/app/src/main/java/com/example/mobilepedalpiapp/PedalComposable.kt
+++ MobilePedalPiApp/app/src/main/java/com/example/mobilepedalpiapp/PedalComposable.kt
@@ -1,11 +1,9 @@
 package com.example.mobilepedalpiapp
 
 import android.media.Image
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -13,30 +11,47 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.example.mobilepedalpiapp.ui.theme.DarkRed
+import com.example.mobilepedalpiapp.ui.theme.DarkerGrey
 
 @Composable
 fun PedalComposable() {
+    Box {
+        Image(
+            painter = painterResource(id = R.drawable.pedal_template),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth(0.8f)
+                .fillMaxHeight(0.9f),
+        )
+        SwitchComposable()
+    }
+}
+
+@Composable
+fun SwitchComposable() {
     Box(
         modifier = Modifier
-            .fillMaxWidth(0.7f)
-            .fillMaxHeight(0.6f)
-            .clip(RoundedCornerShape(12.dp))
-            .background(
-                brush = Brush.horizontalGradient(
-                    0.0f to Color.Black,
-                    0.05f to Color.DarkGray,
-                    0.1f to Color.LightGray,
+            .fillMaxWidth(0.6f)
+            .fillMaxHeight(0.2f)
+            .absoluteOffset(x = 40.dp, y = 440.dp)
+            .padding(bottom = 25.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.black_plastic),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxSize()
+                .clip(RoundedCornerShape(10.dp)),
+            contentScale = ContentScale.FillBounds
+        )
+    }
+}
 
-                    0.95f to Color.DarkGray,
-                    1.0f to Color.Black,
-                    startX = 0f,
-                    endX = Float.POSITIVE_INFINITY
-                )
-            ),
-        contentAlignment = Alignment.Center
-    ) {}
-
+@Composable
+fun KnobComposable() {
 
 }
